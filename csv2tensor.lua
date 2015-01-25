dd67cf25724bbe2a07549bdbb6ef91f1c0a36918
Will Kurt <wckurt@gmail.com>
2015-01-25 14:15:23 -0800

    pulled out code into module

--- csv2tensor.lua
+++ csv2tensor.lua
@@ -1,31 +1,9 @@
 require 'csvigo'
 require 'torch'
 
-function csv2tensor(path,opts)
-   print("loading data from csv")
-   if(opts and opts.exclude and opts.include) then
-      print("Warning: both 'exclude' and 'include' specified. Using 'exclude' only")
-   end
-   local csv_data  = csvigo.load{path=path}
-   local col_names, ncols
-   if opts and opts.exclude then
-      col_names, ncols = _exclude_cols(csv_data,opts.exclude)
-   elseif opts and opts.include then
-      col_names, ncols = _include_cols(csv_data,opts.include)
-   else
-      col_names, ncols = _all_cols(csv_data)
-   end
-   local n_rows = #csv_data[col_names[1]]
-   print("creating tensor")
-   local tensor_data = torch.Tensor(n_rows,#col_names)
-   for i,col_name in ipairs(col_names) do
-      next_col =  torch.Tensor(csv_data[col_name])
-      tensor_data[{{},i}] = next_col
-   end
-   return tensor_data, col_names
-end
+local csv2tensor = {}
 
-function _all_cols(csv_data)
+local function all_cols(csv_data)
    local col_names = {}
    local ncols = 0
    for k,v in pairs(csv_data) do
@@ -35,7 +13,16 @@ function _all_cols(csv_data)
    return col_names, ncols
 end
 
-function _exclude_cols(csv_data,ignore)
+local function _in(s,ls)
+   for _, v in ipairs(ls) do
+      if v == s then
+         return true
+      end
+   end
+   return false
+end
+
+local function exclude_cols(csv_data,ignore)
    local col_names = {}
    local ncols = 0
    for k,v in pairs(csv_data) do
@@ -47,7 +34,7 @@ function _exclude_cols(csv_data,ignore)
    return col_names, ncols
 end
 
-function _include_cols(csv_data,keep)
+local function include_cols(csv_data,keep)
    local col_names = {}
    local ncols = 0
    for k,v in pairs(csv_data) do
@@ -59,11 +46,30 @@ function _include_cols(csv_data,keep)
    return col_names, ncols
 end
 
-function _in(s,ls)
-   for _, v in ipairs(ls) do
-      if v == s then
-         return true
-      end
+
+function csv2tensor.load (path,opts)
+   print("loading data from csv")
+   if(opts and opts.exclude and opts.include) then
+      print("Warning: both 'exclude' and 'include' specified. Using 'exclude' only")
    end
-   return false
+   local csv_data  = csvigo.load{path=path}
+   local col_names, ncols
+   if opts and opts.exclude then
+      col_names, ncols = exclude_cols(csv_data,opts.exclude)
+   elseif opts and opts.include then
+      col_names, ncols = include_cols(csv_data,opts.include)
+   else
+      col_names, ncols = all_cols(csv_data)
+   end
+   local n_rows = #csv_data[col_names[1]]
+   print("creating tensor")
+   local tensor_data = torch.Tensor(n_rows,#col_names)
+   for i,col_name in ipairs(col_names) do
+      next_col =  torch.Tensor(csv_data[col_name])
+      tensor_data[{{},i}] = next_col
+   end
+   return tensor_data, col_names
 end
+
+
+return csv2tensor
--- example.lua
+++ example.lua
@@ -1,14 +1,14 @@
 -- quick example of loading a csv file
-dofile("csv2tensor.lua")
-sample_tensor, col_names = csv2tensor("./data/simple.csv")
+csv2tensor = require 'csv2tensor'
+sample_tensor, col_names = csv2tensor.load("./data/simple.csv")
 print(sample_tensor)
 print(col_names)
 
 
-sample_tensor2, col_names2 = csv2tensor("./data/simple.csv",{exclude={"col_a"}})
+sample_tensor2, col_names2 = csv2tensor.load("./data/simple.csv",{exclude={"col_a"}})
 print(sample_tensor2)
 print(col_names2)
 
-sample_tensor3, col_names3 = csv2tensor("./data/simple.csv",{include={"col_a"}})
+sample_tensor3, col_names3 = csv2tensor.load("./data/simple.csv",{include={"col_a"}})
 print(sample_tensor3)
 print(col_names3)
